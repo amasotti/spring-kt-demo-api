03ab016909e978b4575dd9655146158401cb3a7c
Antonio Masotti <toniomasotti@gmail.com>
2023-03-04 20:54:26 +0100

    init repo from spring initializr

---
+++ src/main/kotlin/com/bikeleasing/antoniotest/demospringktapi/DemospringktapiApplication.kt
@@ -0,0 +1,11 @@
+package com.bikeleasing.antoniotest.demospringktapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemospringktapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemospringktapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/bikeleasing/antoniotest/demospringktapi/DemospringktapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bikeleasing.antoniotest.demospringktapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemospringktapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
